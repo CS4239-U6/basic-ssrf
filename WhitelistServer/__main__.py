d2d1d37a4f6e06a38fa3a2f78b46543fd7611b4b
Wen Junhua <wenjh1998@gmail.com>
2021-11-14 00:27:43 +0800

    fix: add whitelist server to dockerfile

--- WhitelistServer/__main__.py
+++ WhitelistServer/__main__.py
@@ -44,5 +44,4 @@ def index():
 
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=5000)
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python3 ./HiddenLocalServer/__main__.py &
+# python3 ./WhitelistServer/__main__.py
 python3 ./VulnerableServer/__main__.py
